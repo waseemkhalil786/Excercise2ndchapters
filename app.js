04b3bf7f2372b0e894e8c9bc5b3d648dd05ba0fc
Waseem khalil world <156056064+waseemkhalil786@users.noreply.github.com>
2024-03-07 09:57:10 +0500

    abhi exercise rahiti hain  exponents wali

---
+++ app.js
@@ -0,0 +1,63 @@
+// first excercise 
+let str1 = 'Laurence';
+
+let str2 = 'Svekis';
+
+let vall = undefined;
+
+let val2 = null;
+
+let myNum = 1000;
+
+console.log("str1", typeof str1);
+console.log("str2", typeof str2);
+console.log("vall", typeof vall);
+console.log("vall2", typeof val);
+console.log("myNum", typeof myN);
+
+// finish 1 excercise
+
+// 2.2 exercise
+
+// These two value string are not equal is called false!
+
+let name = "Waseem khalil";
+let age = "15";
+
+// Boolean
+
+console.log("These are the!:", name === age )
+
+// These two value string are same value is called true 
+
+let name1 = "Waseem khalil";
+
+let age1 = "Waseem khalil";
+
+console.log("These are the!:", name1 === age1 )
+
+// complet excercise 2.2 !
+
+// Percentage formula
+
+let fees = 50;
+
+let Daily  = 100;
+
+let define = fees % Daily;
+
+console.log(`${fees} % ${Daily} = ${define}`);
+
+// Modualus finish
+
+// ecercise 2.3 start
+
+// A**2 + B**2 = C**2;
+
+let A =3**2;
+let B =2**2;
+let C = 10;
+let define1= A+B
+
+console.log(define1)
+// uncomplete
